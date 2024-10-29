6ea708a70903d97833ea2f93cdae362c0d113bbb
Ahmd Azeri Chandra Bhuana <62881867+ahmadazerichandrabhuana@users.noreply.github.com>
2024-10-29 13:39:48 +0700

    Delete config/chromedriver.py

--- config/chromedriver.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-def get_chromedriver():
-    driver_path = ChromeDriverManager().install()
-    print(driver_path)
-    return  driver_path
